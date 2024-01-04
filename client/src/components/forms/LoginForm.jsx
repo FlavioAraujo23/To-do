b581825f037132c61c192f7f5bb457b610850801
FlavioAraujo23 <flaviolopes1027@gmail.com>
2024-01-04 19:13:38 -0300

    :sparkles: feat: Fetch para realizar login com o back

--- client/src/components/forms/LoginForm.jsx
+++ client/src/components/forms/LoginForm.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react"
 import SubmitButton from "../buttons/SubmitButton"
+import { fetchData } from "../../actions/fetch";
+import toast, { Toaster } from 'react-hot-toast';
+import ToastSuccess from "../customToasted/toastSuccess";
 
 const LoginForm = () => {
+  const [email, setEmail]= useState('');
+  const [senha, setSenha] = useState('');
+  async function handleLogin(e) {
+    e.preventDefault();
+    const url = 'http://localhost:3000/auth/login'
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({email, senha}),
+      }
+    const dados = await fetchData(url, options);
+    console.log(dados)
+
+  }
+
   return (
     <main className='w-screen flex gap-32'>
-      <div className="bg-greenMy w-1/2 h-screen">
+      <div
+        className="w-1/2 h-screen"
+        style={{backgroundColor:'#5AC7AA'}}
+      >
         <div className="ml-5">relogio</div>
         <div className="w-32 text-white ml-5">
           <p>Everything you need to do in one place</p>
@@ -19,24 +41,37 @@ const LoginForm = () => {
             </svg>
           </div>
         </div>
-          <form className="max-w-xs flex flex-col mt-48 gap-4">
+          <form className="max-w-xs flex flex-col mt-48 gap-4" onSubmit={handleLogin}>
             <h2 className="font-bold text-center text-xl">Login</h2>
             <label htmlFor="">
-              <input type="text" placeholder='Email Address' className="border-b w-full"/>
+              <input 
+                type="text"
+                placeholder='Email Address'
+                className="border-b w-full"
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+              />
             </label>
             <label htmlFor="">
-              <input type="password" className="border-b w-full " placeholder='Password'/>
+              <input 
+                type="password" 
+                className="border-b w-full"
+                value={senha}
+                onChange={({ target }) => setSenha(target.value)}
+                placeholder='Password'
+              />
             </label>
             <div className="mt-2">
               <SubmitButton title="Login"/>
             </div>
             <div className="flex relative">
-                <div className="w-full h-12 flex flex-col text-gray/90 pt-1">
+                <div className="w-full h-12 flex flex-col text-gray-300/90 pt-1">
                 <span>Do not have an account yet?</span>
-                <a className="border-b w-14 hover:text-gray/50 cursor-pointer">Sign Up</a>
+                <a className="border-b w-14 hover:text-gray-300/50 cursor-pointer">Sign Up</a>
                 </div>
             </div>
           </form>
+          <Toaster/>
       </div>
     </main>
   )

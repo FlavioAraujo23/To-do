4bacec7357d115b5f5ef2817b5ecd4125a00d4be
FlavioAraujo23 <flaviolopes1027@gmail.com>
2024-01-09 20:28:17 -0300

    :sparkles: feat: Header da homework de listas

--- client/src/components/lists/ListHomework.jsx
+++ client/src/components/lists/ListHomework.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react"
+import AddTodoButton from "../buttons/AddTodoButton"
+import DeleteTodoButton from "../buttons/DeleteTodoButton"
+import InviteTodoButton from "../buttons/InviteTodoButton"
+import InviteModalForm from "../forms/InviteModalForm"
 
 const ListHomework = () => {
+  const [modalInvite, setModalinvite] = useState();
+
+  function handleOpenModal() {
+    setModalinvite(true);
+  }
+
+  function handleCloseModal() {
+    setModalinvite(false);
+  }
+
   return (
-    <div>ListHomework</div>
+    <main className="container">
+      <header className="flex justify-between items-center pt-4 pl-4">
+        <h2 className="font-bold text-xl">Homework</h2>
+        <div className="flex items-center gap-2 pr-4">
+          <div className="pr-4">
+            <DeleteTodoButton />
+          </div>
+          <AddTodoButton />
+          <InviteTodoButton onClick={handleOpenModal} />
+        </div>
+       
+      </header>
+       {modalInvite && <InviteModalForm estadoModal={modalInvite} fecharModal={handleCloseModal}/>}
+    </main>
   )
 }
 

d9f94d7f7462ecf982b465c399a8eb37c026a16d
FlavioAraujo23 <flaviolopes1027@gmail.com>
2024-01-24 19:54:29 -0300

    :sparkles: feat: middleware para erros de requisição de api

--- client/src/actions/fetch.js
+++ client/src/actions/fetch.js
@@ -1,5 +1,11 @@
 export async function fetchData(url, options) {
   const response = await fetch(url, options);
   const data = await response.json();
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.error);
+  }
+
   return data;
 }
--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(bodyParser.json());
 app.use('/auth', authController);
 app.use('/list', listController);
 
+// Middleware de tratamento de erros
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  // Verifica se o erro é do tipo 'ValidationError'
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message });
+  }
+  
+  res.status(500).json({ error: 'Erro interno do servidor' });
+});
+
 app.listen(PORT, () => {
   console.log(`servidor rodando na porta ${PORT}`);
 })

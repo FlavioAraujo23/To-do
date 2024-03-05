e56c9c12b853e0924a8bb22462c3137050a62a0c
FlavioAraujo23 <flaviolopes1027@gmail.com>
2024-03-04 22:03:00 -0300

    feat: add analytics vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,23 @@ import LoginForm from "./components/forms/LoginForm"
 import { UserStorage } from "./context/UserContext";
 import ListView from "./components/lists/ListView";
 import CreateAccountForm from "./components/forms/CreateAccountForm";
-
-
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <UserStorage>
-          <Routes>
-            <Route path="/" element={ <ListView /> } />
-            <Route path="/login" element={ <LoginForm /> } />
-            <Route path="/signUp" element={ <CreateAccountForm /> } />
-            <Route path="/list" element={ <ListView /> } />
-          </Routes>
-        </UserStorage>
-      </BrowserRouter>
+      <Analytics>
+        <BrowserRouter>
+          <UserStorage>
+            <Routes>
+              <Route path="/" element={ <ListView /> } />
+              <Route path="/login" element={ <LoginForm /> } />
+              <Route path="/signUp" element={ <CreateAccountForm /> } />
+              <Route path="/list" element={ <ListView /> } />
+            </Routes>
+          </UserStorage>
+        </BrowserRouter>
+      </Analytics>
     </div>
   )
 }
